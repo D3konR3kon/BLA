b242fe2448e7a25f8fa30d8e1f32606ff4f78dc0
D3konR3kon <dilavuyo@outlook.com>
2022-05-11 16:26:41 +0200

    My BMI

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload = () => {
+	let button = document.querySelector("#btn");
+
+	// Function for calculating BMI
+	button.addEventListener("click", calculateBMI);
+};
+
+function calculateBMI() {
+
+	/* Getting input from user into height variable.
+	Input is string so typecasting is necessary. */
+	let height = parseInt(document
+			.querySelector("#height").value);
+
+	/* Getting input from user into weight variable.
+	Input is string so typecasting is necessary.*/
+	let weight = parseInt(document
+			.querySelector("#weight").value);
+
+	
+    let result = weight/(height*height);
+    //console.log(result.toFixed(2));
+    document.getElementById("output").value += result;
+    }
